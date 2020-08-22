08fea15178dde90c0c36f2e74b722da2d2962109
phuc <phuctv95@gmail.com>
2020-08-22 12:44:19 +0700

    update header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
-  
-  @Input() title: string;
 
   constructor() { }
 
